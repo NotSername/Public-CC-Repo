85b3fbb1f844bcf6884789fa7e2adccfe02f0f6b
Sername <55925353+NotSername@users.noreply.github.com>
2025-03-18 15:23:11 +0100

    move to its own folder

--- APIs/GUI/GUI.lua
+++ APIs/GUI/GUI.lua
@@ -1,35 +1,3 @@
-
---[[
-
-Simple GUI API since we hate GUIs
-
-Here some example code
-
--------------------------------------------------------------------------------
-local GUI = require("GUI")
-
-GUI.addMenu("Main")
-GUI.addMenu("Settings")
-
-GUI.addButton("Main", "Play", 2, 2, 10, 3, function() print("Playing!") end)
-GUI.addButton("Main", "Settings", 2, 4, 10, 5, function()
-    GUI.setMenu("Settings")
-end)
-GUI.addButton("Main", "Exit", 2, 6, 10, 7, function() os.exit() end)
-
-GUI.addButton("Settings", "Back", 2, 2, 10, 3, function()
-    GUI.setMenu("Main")
-end)
-GUI.addButton("Settings", "Toggle Sound", 2, 4, 10, 5, function()
-    print("Sound toggled!")
-end)
-
-GUI.setMenu("Main")
-GUI.render()
-GUI.start()
--------------------------------------------------------------------------------
---]]
-
 local GUI = {}
 
 local Menus = {}
